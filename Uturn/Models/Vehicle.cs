bd771ddf940e66961bb31c0fe8ec8d1f52c691a5
johanngeustyn <info@johanngeustyn.co.za>
2023-10-31 16:43:44 +0200

    Initial Commit

---
+++ Uturn/Models/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uturn.Models
+{
+	public class Vehicle
+	{
+        public string key { get; set; } = "";
+
+        [Required]
+        public string model { get; set; } = "";
+
+        [Required]
+        public string brand { get; set; } = "";
+
+        [Required]
+        public string type { get; set; } = "";
+
+        [Required]
+        public string numberPlate { get; set; } = "";
+
+        public string currentLocation { get; set; } = "";
+
+        public bool isAvailable { get; set; } = true;
+    }
+}
+
